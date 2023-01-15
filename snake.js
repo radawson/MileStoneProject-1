d53dbe4531a6a84ed330f775819e22cf439f1145
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-01-15 14:39:06 -0800

    made an array to keep track of joints added to the snake at which coordinate

--- snake.js
+++ snake.js
@@ -1,5 +1,5 @@
 
-let canvas;
+//let canvas;
 
 
 let head;
@@ -10,21 +10,25 @@ let dots;
 let mouse_x;
 let mouse_y;
 
+//snake can only move left
 let leftDirection = true;
 let rightDirection = false;
 let upDirection = false;
 let downDirection = false;
 let inGame = true;
 
-const DOT_SIZE = 20; // size of mouse and snake body length
-const ALL_DOTS = 1800; // max amount of dots that can generate in grid
-const MAX_RANDOM = 58; // calculation to position the mouse at random
-const DELAY = 150;  // speed of game
-
-const 
-const
-const
-const
+const dot_size = 20; // size of mouse and snake body length
+const all_dots = 1800; // max amount of dots that can generate in grid
+const max_random = 58; // calculation to position the mouse at random
+const delay = 150;  // speed of game
 
 
+// according to stackoverflow.com to detect ARROW KEY presses use the following keycodes:
+const left_key = 37;
+const right_key = 39;
+const up_key = 38;
+const down_key = 40;
 
+//putting snake joint coordinates in an array
+let x = snakeJoints(all_dots);
+let y = snakeJoints(all_dots);
