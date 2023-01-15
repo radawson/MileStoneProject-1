f1aee5750ae3c2f8a8d47643d3e2bf0bf1361f53
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-01-15 14:31:46 -0800

    defined snake food, body, and direction.
    Currently keys can only move snake to the left

--- snake.js
+++ snake.js
@@ -1,3 +1,30 @@
-<body onload ="startGame()">
+
+let canvas;
+
+
+let head;
+let mouse;
+let ball;
+
+let dots;
+let mouse_x;
+let mouse_y;
+
+let leftDirection = true;
+let rightDirection = false;
+let upDirection = false;
+let downDirection = false;
+let inGame = true;
+
+const DOT_SIZE = 20; // size of mouse and snake body length
+const ALL_DOTS = 1800; // max amount of dots that can generate in grid
+const MAX_RANDOM = 58; // calculation to position the mouse at random
+const DELAY = 150;  // speed of game
+
+const 
+const
+const
+const
+
 
 
