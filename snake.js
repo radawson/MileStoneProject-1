8ed7968278f506db7c85ec2650f389b8d7c893bb
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-01-19 21:42:21 -0800

    added on load event listener, constructor, fx to generate snake at random
    random area where the snake will appear

--- snake.js
+++ snake.js
@@ -1,42 +1,83 @@
 
-// let snakeHead;
-// let mouse;
-// let snakeBody;
-
-// let dots;
-// let mouseX;
-// let mouseY;
-
-// //snake can only move left
-// let leftDirection = true;
-// let rightDirection = false;
-// let upDirection = false;
-// let downDirection = false;
-// let inGame = true;
-
-// const dotSize = 20; // size of mouse and snake body length
-// const allDots = 1800; // max amount of dots that can generate in grid
-// const maxRandom = 58; // calculation to position the mouse at random
-// const delay = 150;  // speed of game
-
-
-// // according to stackoverflow.com to detect ARROW KEY presses use the following keycodes:
-
-// const leftKey = 37;
-// const rightKey = 39;
-// const upKey = 38;
-// const downKey = 40;
-
-
-// function createSnake(){
-//     createElement()
-// }
-
-// //window.reload
-
 //DEFINE NEW VARIABLES snake, mouse, score board, etc
 
 
 let scoreBoard = document.querySelector(".scoreBoard")
 let grid = document.querySelector(".grid");
-let flash = document.querySelector(".restartGame")
+let flash = document.querySelector("flash");
+let restartGame = document.getElementById("restartGame");
+let newGame = document.getElementById("newGame");
+
+
+//make our grid 10 by 10
+
+let width = 10;
+
+// how to track score
+let score = 0;
+
+//how to track mice intake (point accumulation)
+let mouseIndex = 0;
+
+// how fast should the game go? Snake move? 
+let speed = 0.5;
+
+
+//write a function that listens (eventListener) for clicks. Specifically when someone clicks "StartGame"
+
+//window onload
+window.addEventListener("load", () => {
+    console.log("page is fully loaded");
+});
+
+
+class Snake {
+
+    constructor(name, positionX, positionY) {
+        this.name = name;
+        this.positionX = positionX
+        this.positionY = positionY
+    }
+
+
+    sayHi() {
+        alert(this.name);
+    }
+
+    newSnake() {
+        alert(this.positionX, positionY)
+    }
+
+
+}
+
+// Usage:
+let snake = new Snake("John");
+//snake.sayHi()
+
+//let snakeTwo = new Snake("Meroni", 0,10)
+
+//snakeTwo.newSnake()
+
+//let snakeThree = new Snake("Larky", "4,16")
+//snakeThree.sayHi()
+//snakeThree.newSnake()
+
+let snakeFour = new Snake("SuperLarky", between(0,600),between(0,600))
+
+//snakeFour.newSnake()
+
+//
+
+function between(x, y) {
+    return Math.floor(
+        Math.random() * (y - x) + x
+    )
+}
+
+console.log(
+    between(0, 600)
+)
+
+//attatch image to snake 4 w/ constructor
+
