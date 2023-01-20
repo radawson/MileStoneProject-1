2623c2c1a9b6120b0771a42a70ce27ee67817d32
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-01-19 20:03:44 -0800

    created four div's, focusing on scoreboard,and replay
    Deleted previous code and starting with new simpler ideas.

--- snake.js
+++ snake.js
@@ -7,8 +7,8 @@ let mouse;
 let snakeBody;
 
 let dots;
-let mouse_x;
-let mouse_y;
+let mouseX;
+let mouseY;
 
 //snake can only move left
 let leftDirection = true;
@@ -17,45 +17,22 @@ let upDirection = false;
 let downDirection = false;
 let inGame = true;
 
-const dot_size = 20; // size of mouse and snake body length
-const all_dots = 1800; // max amount of dots that can generate in grid
-const max_random = 58; // calculation to position the mouse at random
+const dotSize = 20; // size of mouse and snake body length
+const allDots = 1800; // max amount of dots that can generate in grid
+const maxRandom = 58; // calculation to position the mouse at random
 const delay = 150;  // speed of game
 
 
 // according to stackoverflow.com to detect ARROW KEY presses use the following keycodes:
-const left_key = 37;
-const right_key = 39;
-const up_key = 38;
-const down_key = 40;
 
-//putting snake joint coordinates in an array
-let x = snakeJoints(all_dots);
-let y = snakeJoints(all_dots);
+const leftKey = 37;
+const rightKey = 39;
+const upKey = 38;
+const downKey = 40;
 
 
-function init() {
-    canvas = document.getElementById("snakeCanvas");
-    // to make a 2D game use getContext 
-    //https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript
-    ctx = canvas.getContext("2d");
-
-    loadImages();
-    createSnake();
-    locateMouse();
-    // set a timeout to start animation
-
+function createSnake(){
+    createElement()
 }
 
-function loadImages() {
-    snakeHead = newImage();
-    snakeHead.src = "Assets/snakehead.png";
-
-    snakeBody = newImage();
-    snakeBody.src = "Assets/snakebody.png";
-
-    mouse = newImage();
-    mouse.src = "Assets/mouserunning.png"
-    
-}
-
+//window.reload
