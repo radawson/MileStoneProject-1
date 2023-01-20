d3e8ea966bd8e149979ee63f84f23d4f42a7ea1e
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-01-19 20:26:26 -0800

    added a class for snake and mouse

--- snake.js
+++ snake.js
@@ -1,38 +1,38 @@
 
-//let canvas;
+// let snakeHead;
+// let mouse;
+// let snakeBody;
+
+// let dots;
+// let mouseX;
+// let mouseY;
+
+// //snake can only move left
+// let leftDirection = true;
+// let rightDirection = false;
+// let upDirection = false;
+// let downDirection = false;
+// let inGame = true;
+
+// const dotSize = 20; // size of mouse and snake body length
+// const allDots = 1800; // max amount of dots that can generate in grid
+// const maxRandom = 58; // calculation to position the mouse at random
+// const delay = 150;  // speed of game
 
 
-let snakeHead;
-let mouse;
-let snakeBody;
+// // according to stackoverflow.com to detect ARROW KEY presses use the following keycodes:
 
-let dots;
-let mouseX;
-let mouseY;
-
-//snake can only move left
-let leftDirection = true;
-let rightDirection = false;
-let upDirection = false;
-let downDirection = false;
-let inGame = true;
-
-const dotSize = 20; // size of mouse and snake body length
-const allDots = 1800; // max amount of dots that can generate in grid
-const maxRandom = 58; // calculation to position the mouse at random
-const delay = 150;  // speed of game
+// const leftKey = 37;
+// const rightKey = 39;
+// const upKey = 38;
+// const downKey = 40;
 
 
-// according to stackoverflow.com to detect ARROW KEY presses use the following keycodes:
+// function createSnake(){
+//     createElement()
+// }
 
-const leftKey = 37;
-const rightKey = 39;
-const upKey = 38;
-const downKey = 40;
+// //window.reload
 
-
-function createSnake(){
-    createElement()
-}
-
-//window.reload
+let grid = document.querySelector(".grid");
+let flash = document.querySelector(".restartGame")
